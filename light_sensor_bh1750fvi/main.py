5af994a2df047fe7403ff02670babfd1dc3e2781
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-03 15:51:00 +0200

    added bh1750fvi

---
+++ light_sensor_bh1750fvi/main.py
@@ -0,0 +1,16 @@
+import time
+from machine import I2C
+import bh1750fvi
+
+
+i2c = I2C(0, I2C.MASTER, baudrate=100000, pins=('P23', 'P22'))
+addr = i2c.scan()
+print(addr)
+addr = addr[0]
+
+light_sensor = bh1750fvi.BH1750FVI(i2c, addr)
+
+while True:
+    lum = light_sensor.read()
+    print(lum)
+    time.sleep(1)
---
+++ sensor-city-delft/README.md
@@ -0,0 +1,2 @@
+# Sensor City Delft
+Repository for the sensor platform code.
