498f0948982759ae2f6453608cc565c01320f27d
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-08 20:32:19 +0200

    add basic workflow

--- custom_wlan_server/main.py
+++ custom_wlan_server/main.py
@@ -1,1 +1,6 @@
 # Do stuff here, such as reading sensor values and sending data
+# call this function with a string to change the SSID of your Lopy
+setup_ap("dynamiot2")
+
+import pycom
+pycom.rgbled(0xff)
