81d84deeb8af9a43a213560f5b5a60fb97fe42d5
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-12 23:07:43 +0200

    update gps

--- gps_neo6m/main.py
+++ gps_neo6m/main.py
@@ -1,3 +1,7 @@
+"""
+TXD: P3 = G11
+RXD: P4 = G24
+"""
 import time
 import gps
 import machine
@@ -9,21 +13,15 @@ print('start main')
 time.sleep(5)
 
 # connect to GPS device
-com = GPS_UART_start()
-now = RTC()
-now.init((2017, 5, 12, 12, 30, 0, 0, 0))
+gps = GPS_UART_start()
 
 while True:
-    # print str(place.longitude * 100000),  str(place.latitude * 100000)
     time.sleep(5)
-    if (com.any()):
-        data = com.readline()
-        #print (data)
+    if (gps.any()):
+        data = gps.readline()
 
         if (data[0:6] == b'$GPGGA'):
             place = NmeaParser()
             place.update(data)
-            print (place.latitude, place.longitude)
-
-            # wait a random amount of time
-            time.sleep(10 + (machine.rng() & 0x3f) / 10)
+            if place.latitude != 0 or place.longitude != 0:
+                print (str(place.latitude) + ', ' + str(place.longitude))
