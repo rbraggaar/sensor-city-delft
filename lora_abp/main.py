98d24e04afff217d6b775e0c8130546f476b6a46
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-12 21:21:32 +0200

    set socket blocking

--- lora_abp/main.py
+++ lora_abp/main.py
@@ -15,14 +15,8 @@ print('Main start')
 
 # LoRa details keys obtained from KPN
 dev_addr = struct.unpack(">l", binascii.unhexlify(config.DEV_ADDR))[0]
-print(dev_addr)
-# manually converted hex to decimal for better readability
-#dev_addr = 337656918
-
 nwks_key = binascii.unhexlify(config.NWKS_KEY)
-print(nwks_key)
 apps_key = binascii.unhexlify(config.APPS_KEY)
-print(apps_key)
 
 # Setup LoRa
 lora = LoRa(mode=LoRa.LORAWAN, adr=True)
@@ -47,17 +41,22 @@ def ack():
 
 
 def lora_tx(payload):
+    lora_sock.setblocking(True)
     print('Sending uplink message')
     pycom.rgbled(0xff0000)
     lora_sock.send(payload)
     print('LoRa uplink complete')
     ack()
+    lora_sock.setblocking(False)
+    data = lora_sock.recv(64)
+    print(data)
 
 
-# call this function to send one byte: 0A as hex
+# call this function to send 'hi' as raw byte data (hex format)
 def send_hi_bytes():
     lora_tx(bytes([0x68, 0x69]))
 
 
+# call this function to send 'hi' as string through the socket
 def send_hi_string():
     lora_tx('hi')
