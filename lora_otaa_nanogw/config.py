6e3b9dddf912d5316a00edd3f6fda22e650fc0f1
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-19 11:20:28 +0200

    LoRa OTAA example compatible with Lopy nano gw

---
+++ lora_otaa_nanogw/config.py
@@ -0,0 +1,7 @@
+"""
+Change these for your own keys (see https://www.thethingsnetwork.org)
+Type has to be string
+"""
+DEV_EUI = '<replace with your values>'
+APP_EUI = '<replace with your values>'
+APP_KEY = '<replace with your values>'
--- lora_otaa_nanogw/main.py
+++ lora_otaa_nanogw/main.py
@@ -1,24 +1,34 @@
 """Basic LoRa example based on OTAA (Over The Air Authentication)
 Setup your TTN details in config.py
+
+Special LoPy-node script compatible with the LoPy Nano Gateway
 """
-import time
+
 from network import LoRa
 import socket
 import binascii
+import struct
+import time
 import config
-import pycom
 
 
 print('Main start')
 
 # Setup LoRa
-lora = LoRa(mode=LoRa.LORAWAN, adr=True)
+lora = LoRa(mode=LoRa.LORAWAN)
 
+dev_eui = binascii.unhexlify(config.DEV_EUI)
 app_eui = binascii.unhexlify(config.APP_EUI)
 app_key = binascii.unhexlify(config.APP_KEY)
 
+# set the 3 default channels to the same frequency
+# (must be before sending the OTAA join request)
+lora.add_channel(0, frequency=868100000, dr_min=0, dr_max=5)
+lora.add_channel(1, frequency=868100000, dr_min=0, dr_max=5)
+lora.add_channel(2, frequency=868100000, dr_min=0, dr_max=5)
+
 # join a LoRa network using OTAA
-lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
 # wait until the module has joined the network
 print('Joining LoRa...')
@@ -31,13 +41,20 @@ pycom.rgbled(0)
 print("LoRa joined")
 print("LoRa active: ", lora.has_joined())
 
+# remove all the non-default channels
+for i in range(3, 16):
+    lora.remove_channel(i)
+
 # create a LoRa socket
 lora_sock = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 # set the LoRaWAN data rate
 lora_sock.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
+# make the socket blocking
 lora_sock.setblocking(False)
+
+time.sleep(5.0)
 
 
 def ack():
