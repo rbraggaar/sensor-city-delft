90c5cedf9e8866eb945b2dcf562d4047c557d34d
Rob Braggaar <r.c.braggaar@outlook.com>
2017-06-04 13:00:45 +0200

    formatting

--- airqual_ppd42ns/main.py
+++ airqual_ppd42ns/main.py
@@ -28,11 +28,11 @@ ppd_pin = Pin(INPUT_PIN, mode=Pin.IN)
 chrono = Timer.Chrono()
 
 while True:
-    # start time of new sampling window in seconds
+    # start time of sampling window in seconds
     starttime = time.time()
     # ratio of LPO time over the entire sampling window
     ratio = 0
-    #  Lo Pulse Occupancy Time (LPO Time) in microseconds
+    #  Low Pulse Occupancy Time (LPO Time) in microseconds
     low_pulse_occ = 0
     # concentration based on LPO time and characteristics graph (datasheet)
     concentration = 0
@@ -40,13 +40,15 @@ while True:
     while time.time() - starttime <= SAMPLETIME_S:  # in sampling window
         # check if pin is low, start timing
         if ppd_pin.value() == 0:
-            chrono.start()  # if it is already started it will continue counting
+            # if it is already started it will continue counting
+            chrono.start()
         # get duration of low pulse and reset timer
         else:
             low_pulse_occ += chrono.read_us()
             chrono.reset()
-    # ratio: integer percentage (0 - 100%)
-    ratio = low_pulse_occ / (SAMPLETIME_S * 1000000.0)
-    concentration = 2.1 * (ratio ** 3) - 3.8 * (ratio ** 2) + 520 * ratio + 0.62
+
+    # ratio: percentage of low pulses over the sampling window
+    concentration = 1.1 * (ratio ** 3) - 3.8 * (ratio ** 2) + 520 * ratio + 0.62
+
     if concentration != 0.62:
-        print("Concentration is {} pcs/0.01cf".format(concentration))
+        print("PM concentration: {} pcs/0.01cf".format(concentration))
